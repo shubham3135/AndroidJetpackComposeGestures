4ac59953cc2260cbeb542658ed4264899d6a48e9
Shubham <shubhamkumarwinner@gmail.com>
2021-08-16 18:21:23 +0530

    Scrolling

--- app/src/main/java/com/shubhamkumarwinner/composegestures/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composegestures/MainActivity.kt
@@ -3,18 +3,23 @@ package com.shubhamkumarwinner.composegestures
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shubhamkumarwinner.composegestures.ui.theme.ComposeGesturesTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,13 +29,70 @@ class MainActivity : ComponentActivity() {
             ComposeGesturesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ClickableSample()
+                    ScrollableSample()
                 }
             }
         }
     }
 }
 
+//scrolling
+@Composable
+fun ScrollBoxes() {
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .size(300.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        repeat(20) {
+            Text("Item $it", modifier = Modifier.padding(10.dp))
+        }
+    }
+}
+//scrolling
+@Composable
+private fun ScrollBoxesSmooth() {
+
+    // Smoothly scroll 100px on first composition
+    val state = rememberScrollState()
+    LaunchedEffect(Unit) { state.animateScrollTo(100) }
+
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .size(300.dp)
+            .padding(horizontal = 8.dp)
+            .verticalScroll(state)
+    ) {
+        repeat(20) {
+            Text("Item $it", modifier = Modifier.padding(10.dp))
+        }
+    }
+}
+//scrolling
+@Composable
+fun ScrollableSample() {
+    // actual composable state
+    var offset by remember { mutableStateOf(0f) }
+    Box(
+        Modifier
+            .size(150.dp)
+            .scrollable(
+                orientation = Orientation.Vertical,
+                // Scrollable state: describes how to consume
+                // scrolling delta and update offset
+                state = rememberScrollableState { delta ->
+                    offset += delta
+                    delta
+                }
+            )
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(offset.toString())
+    }
+}
 
 //tapping and pressing
 @Composable
